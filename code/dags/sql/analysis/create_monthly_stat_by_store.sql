447e5487b6bb97d3d71227de7529251a1af7b4f1
sanghoon <shinsanghooon@gmail.com>
2023-11-02 10:03:17 +0900

    sql 파일 수정

--- code/dags/sql/analysis/create_monthly_stat_by_store.sql
+++ code/dags/sql/analysis/create_monthly_stat_by_store.sql
@@ -1,9 +1,9 @@
 create table if not exists "torder".monthly_stat_by_store as (
     select store_code
-        ,to_date(to_char(business_day, 'yyyy-mm-dd'), 'yyyy-mm')
-        , sum(order_count) as order_count_all
-        , sum(error_count) as error_count_all 
-        , 1.0 * sum(error_count) / sum(order_count) as error_rate_all
+        ,to_date(to_char(business_day, 'yyyy-mm-dd'), 'yyyy-mm') as business_month
+        , sum(order_count) as order_count
+        , sum(error_count) as error_count
+        , 1.0 * sum(error_count) / sum(order_count) as error_rate
     from "torder".daily_stat 
     group by store_code, to_date(to_char(business_day, 'yyyy-mm-dd'), 'yyyy-mm')
 );
--- code/dags/sql/analysis/create_monthly_stat_overview.sql
+++ code/dags/sql/analysis/create_monthly_stat_overview.sql
@@ -1,5 +1,5 @@
 create table if not exists "torder".monthly_stat_overview as (
-    select to_date(to_char(business_day, 'yyyy-mm-dd'), 'yyyy-mm')
+    select to_date(to_char(business_day, 'yyyy-mm-dd'), 'yyyy-mm') as business_month
         , sum(order_count) as order_count_all
         , sum(error_count) as error_count_all 
         , 1.0 * sum(error_count) / sum(order_count) as error_rate_all
